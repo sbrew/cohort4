8d1aa080a33598729d715b2beaee279b2374c7d0
steve B <stephenbrousseau89@gmail.com>
2020-02-24 11:57:56 -0700

    commit 100b

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -2,6 +2,7 @@
 const functions = {
     
     size: (num) => {
+        if (num < -2) return "extra large";
         if (num < 10) return "small";
         if (num < 20) return "medium";
         return "large";
@@ -13,8 +14,11 @@ const functions = {
 
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
+    },
 
+    isEven: (num)=>{
+        return false
+    }
     
 };
 
