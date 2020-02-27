03b4782ddea29472dddf6d89ad243f09db6c4c7f
steve B <stephenbrousseau89@gmail.com>
2020-02-27 14:29:02 -0700

    100C 1st section near completion

--- 01-getting-started/src/scripts/syntax.js
+++ 01-getting-started/src/scripts/syntax.js
@@ -1,24 +1,140 @@
-define attributes / variables
-number
-string
-boolean
-array
-dictionary / objects
-undefined
-sample if / else
-functions
-parameters
-returns
-arrays
-add to the front
-add to the end
-update values
-loops 
-for
-for/in
-while
-do while
-forEach (with array and function)
-Objects / Dictionaries
-declare object
-lookup key to retrieve value
+const functions = {
+// define attributes / variables
+// -number
+//      isNumber
+//      input Number
+//      output number
+    isNum: (num) => {
+    return num +2 
+    },
+// -string
+        // isString
+        // input string
+        // output string
+    isString: (str) =>{
+        return str +" time"
+    },
+// -boolean
+    isTrue: (age) =>{
+    if (age>=18) return true;
+    return false;
+    },
+// -array
+    updateArray: (num) =>{
+        let arr = [69, 0, 69]
+        arr [1] = num
+        return arr;
+    },
+// -dictionary / objects
+    updateObject: (type) =>{
+        var vehicle = {
+            brand : "Dodge",
+            model : type,
+            fuel  : "diesel"
+          };
+          return vehicle.model;
+    },
+// -undefined
+    isUndefined: () =>{
+        var x;
+// if (x === undefined) {return x;
+  // these statements execute
+// }
+// else {
+  // these statements do not execute
+// }  Everything is commented out because KISS 
+    },
+
+
+
+// sample if / else
+    // name isEven
+    // input number
+    // output number
+
+    isEven: (num) =>{
+        if (num %2 === 0) {return true};
+         {return false};
+    },
+    
+
+// functions
+// -parameters
+isPar: (num) => {
+    var x = num;
+    return num;
+},
+// -returns
+
+// arrays
+// -add to the front 
+  frontArray: (num) =>{
+     var arr1 = [2, 3, 4, 5];
+      arr1.unshift(num);
+      return arr1;
+},
+// -add to the end
+    backArray: (num) =>{
+        var arr = [1, 2, 3, 4];
+        arr.push(num);
+        return arr;
+    },
+// -update values
+updArr: (num) =>{
+    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits[num] = "Kiwi";
+    return fruits;
+},
+// loops 
+// -for
+isLoop: (num) =>{
+  
+   for ( let i=0; i<5; i++) {
+   num = num +i;
+    }
+return num;
+},
+
+// -for/in
+inLoop: (y) =>{
+    var txt = "";
+    var x;
+    // dont hard code the parameters into the function, 
+    // they will be read from the recieved part of the test
+    // y doesnt need to be defined as a variable as it is already declared in the parameter
+    for (x in y) {
+      txt += y[x];
+    }
+    return txt;
+    // requires the return 
+},
+// -while
+isWhile: (num) => {
+    let i = 0;
+    while (i < 5) {
+        num = num +i;
+        i++;
+    }
+    return num;
+},
+
+// -do while
+
+doWhile: (num) => {
+    let i = 0;
+    do {num = num +i;
+    i++;
+} 
+    while (i < 5);
+    return num;
+}
+
+// -forEach (with array and function)
+//  advised to skip as these are horrible to do
+
+// Objects / Dictionaries
+// -declare object
+// -lookup key to retrieve value
+};
+
+export default functions;
