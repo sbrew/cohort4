f763a9c3ec9cf07beb8b2eda19cd1e1622f2f9ad
steve B <stephenbrousseau89@gmail.com>
2020-04-10 09:39:50 -0600

    updates

---
+++ 01-getting-started/javascript/daily.js
@@ -0,0 +1,13 @@
+const daily = {  //this creates the object
+    
+    
+    
+    convertToFahrenheit: (tempCelcius) => { //this is the stub function, useful as it is a guide to how it should work
+        
+        return ((tempCelcius*1.8)+32);
+        // when writing minimum amount of code, can just use return the expected value
+        //lookup and practice edge cases
+    }
+};
+
+export default daily;
