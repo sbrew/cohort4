b1d4d2a4af44c4b32687350a7fa9e096b6323d1c
steve B <stephenbrousseau89@gmail.com>
2020-06-09 15:08:24 -0600

    built remove function and total function, moving on to React Component

--- react-02/src/buisness/index.test.js
+++ react-02/src/buisness/index.test.js
@@ -31,12 +31,45 @@ test('does it insert a list item in the next node', () => {
     expect(ll.tail.show()).toEqual("2 & 2");
     expect(ll.head.show()).toEqual("3 & 3");   
     ll.goToHeadNode();
-    // ll.movingBackward();
     expect(ll.currentNode).toEqual(null); 
     ll.insertAtNext(5,5);
     expect(ll.size).toBe(5);
     expect(ll.head.show()).toEqual("5 & 5");
     expect(ll.currentNode.forwardNode.show()).toEqual("3 & 3");   
+});
+
+test('does it delete the listnode', () => {
+    const ll = new linkListStuff.LinkedList();
+    ll.insertAtNext(3,3);
+    ll.insertAtNext(2,2);
+    ll.movingBackward();
+    ll.removeCurrent();
+    expect(ll.size).toBe(1);
+    expect(ll.head.show()).toEqual("2 & 2");
+    ll.insertAtNext(1,1);
+    expect(ll.size).toBe(2);
+    expect(ll.tail.show()).toEqual("1 & 1");
+    ll.removeCurrent();
+    expect(ll.size).toBe(1);
+    expect(ll.tail.show()).toEqual("2 & 2");
+    ll.insertAtNext(3,3);
+    ll.insertAtNext(4,4);
+    expect(ll.size).toBe(3);
+    ll.movingBackward();
+    expect(ll.currentNode.show()).toEqual("3 & 3");
+    ll.removeCurrent();
+    expect(ll.size).toBe(2);
+    expect(ll.currentNode.show()).toEqual("2 & 2");
+    expect(ll.head.show()).toEqual("2 & 2");
+    expect(ll.tail.show()).toEqual("4 & 4");
+});
+
+test('does it return the total of all amounts', () =>{
+    const ll = new linkListStuff.LinkedList();
+    ll.insertAtNext(3,3);
+    ll.insertAtNext(2,2);
+    ll.insertAtNext(1,1);
+    expect(ll.totalFunction()).toBe(6);
 });
 
 
