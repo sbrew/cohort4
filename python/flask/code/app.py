5628c61ece81ec4de6a234ae5130da83342a24e9
steve B <stephenbrousseau89@gmail.com>
2020-07-15 16:16:47 -0600

    Added advanced request parsing

--- python/flask/code/app.py
+++ python/flask/code/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = 'supersecretpassword'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # creates new end point /auth
+jwt = JWT(app, authenticate, identity)  # creates new end point /auth
 
 items = []
+
 
 class Item(Resource):
     @jwt_required()
@@ -19,16 +20,16 @@ class Item(Resource):
         #     if item['name'] == name:
         #         return item
         #     else:
-                # return {'item': None}, 404
-        item = next(filter(lambda x: x['name'] == name, items ), None)
+        # return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items ), None) is not None:
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = request.get_json()
-        item={'name': name, 'price': data['price']}
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
@@ -38,7 +39,14 @@ class Item(Resource):
         return {'message': 'Item Deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This Field cannot be left blank"
+        )
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
@@ -51,7 +59,6 @@ class Item(Resource):
 class ItemList(Resource):
     def get(self):
         return {'items': items}
-
 
 
 api.add_resource(Item, '/item/<string:name>')
