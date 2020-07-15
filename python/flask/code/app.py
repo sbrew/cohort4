6b56f52ef27c78b965c69be42dd93cdea9615700
steve B <stephenbrousseau89@gmail.com>
2020-07-15 15:15:12 -0600

    implemented authentication

--- python/flask/code/app.py
+++ python/flask/code/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app=Flask(__name__)
 app.secret_key = 'supersecretpassword'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) # creates new end point /auth
+
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # for item in items:
         #     if item['name'] == name:
