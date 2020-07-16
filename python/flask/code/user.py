ae7da5f166fca5a007c6bafb279244d5682b3353
steve B <stephenbrousseau89@gmail.com>
2020-07-16 10:31:09 -0600

    added user registration

--- python/flask/code/user.py
+++ python/flask/code/user.py
@@ -1,6 +1,8 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
+
     def __init__(self, _id, username, password):
         self.id = _id #using underscore because id is a python keyword
         self.username = username
@@ -44,3 +46,30 @@ class User:
 
         connection.close()
         return user
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+            type=str,
+            required=True,
+            help="Username Field cannot be left blank"
+        )
+    parser.add_argument('password',
+            type=str,
+            required=True,
+            help="Password Field cannot be left blank.. would you just leave your doors unlocked??"
+        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully."},201
