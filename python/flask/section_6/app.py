dcb356fda5cca906a7e6b21d53b9daaaf1fdfb2a
steve B <stephenbrousseau89@gmail.com>
2020-07-17 14:51:12 -0600

    added store model and store resource

--- python/flask/section_6/app.py
+++ python/flask/section_6/app.py
@@ -6,6 +6,8 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -19,6 +21,8 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)  # creates new end point /auth
 
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
--- python/flask/section_6/resources/item.py
+++ python/flask/section_6/resources/item.py
@@ -9,6 +9,11 @@ class Item(Resource):
             required=True,
             help="This Field cannot be left blank"
         )
+    parser.add_argument('store_id',
+            type=int,
+            required=True,
+            help="what store the items are found in"
+        )
         
     @jwt_required()
     def get(self, name):
@@ -24,7 +29,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -48,7 +53,7 @@ class Item(Resource):
 
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, data['price'], data['store_id'])
         else:
             item.price = data['price']
 
