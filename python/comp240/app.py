1b7b384d440d2bd1adbb217764cb50953147862e
steve B <stephenbrousseau89@gmail.com>
2020-07-21 13:38:37 -0600

    added templates

--- python/comp240/app.py
+++ python/comp240/app.py
@@ -1,16 +1,21 @@
-from flask import Flask, jsonify
-from excel_invoices import clientInvoices
-
+from flask import Flask, jsonify, render_template
+from excel_invoices import clientInvoices, dictionaryBuilder
 
 app = Flask(__name__)
 
+@app.route('/')  # This refers to the homepage
+def home():
+    return render_template("base.html")
 
+@app.route("/dump")
+def simpleDump():
+    data = dictionaryBuilder()
+    return jsonify(data)
 
 
 @app.route('/invoice/<int:invoice_no>')
 def invoice(invoice_no):
     return jsonify({'Invoice Report': clientInvoices(invoice_no)}), 200
-
 
 
 app.run(port=5000)
